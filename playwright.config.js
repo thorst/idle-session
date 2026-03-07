9acf731ea56b023d09da53036ae6ff53a60c647a
Todd Horst <toddmhorst@yahoo.com>
2026-03-07 16:03:52 -0500

    Fix test

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
     testDir: './tests',
-    testIgnore: ['**/smoke.spec.js', '**/fixtures.js', '**/global.setup.js'],
+    testIgnore: ['**/fixtures.js', '**/global.setup.js'],
     use: {
         baseURL: 'http://localhost:5173',
     },
